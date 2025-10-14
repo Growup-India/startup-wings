1e6ca728bcba32d13258aebb92fd1e46d5b8c720
Yash Dixit <yashdixit475@gmail.com>
2025-10-14 14:48:14 +0530

    fix error

--- Back-end/server.js
+++ Back-end/server.js
@@ -23,7 +23,7 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
